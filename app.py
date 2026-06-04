8d71b7d1ff7d34d4aa08b057a50c5fd1f675a7fa
anggie-stack <giienauli@gmail.com>
2026-06-04 20:29:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,3 +69,14 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("Home.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
